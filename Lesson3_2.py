355f94c48669a8e9b6b43c1e7fd2b301344deb12
litvinenko630 <57844940+litvinenko630@users.noreply.github.com>
2021-06-30 16:37:51 +0300

    Delete Lesson3_2.py

--- Lesson3_2.py
+++
@@ -1,9 +0,0 @@
-
-def my_func(**kwargs):
-    """создаем словарь именованных аргументов конвертируем их в список по значениям"""
-    my_data = kwargs
-    return list(my_data.values())
-
-
-print(my_func(name="Sergey", second_name="Litvinenko", year_of_birth=1983, city="Moscow",
-              email="litvinenko12345@yandex.ru", phone_number=89651000000))
