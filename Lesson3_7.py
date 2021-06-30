28e6342350983e3ade7396b2a2a4acc845380168
litvinenko630 <57844940+litvinenko630@users.noreply.github.com>
2021-06-30 16:36:50 +0300

    Delete Lesson3_7.py

--- Lesson3_7.py
+++
@@ -1,8 +0,0 @@
-
-def int_func(letter):
-    """создаем переменную для помещения значения"""
-    word = letter.title()
-    return word
-
-
-print(int_func(input("Enter some words in lowercase spaced by one: ")))
