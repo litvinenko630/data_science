20c85a77bec6ff8a5335dba21f13799198bbe30f
litvinenko630 <57844940+litvinenko630@users.noreply.github.com>
2021-06-30 16:35:17 +0300

    Delete Lesson1_2.py

--- Lesson1_2.py
+++
@@ -1,5 +0,0 @@
-total = int(input("Введите количество секунд: "))
-seconds = total % 60
-minutes = int(((total - seconds) / 60) % 60)
-hours = int((total - seconds - minutes * 60) / 3600)
-print(f"{hours:02}:{minutes:02}:{seconds:02}")
